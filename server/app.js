41a45ac56da4cea30e37525ab9757b7315db1a44
kwonh11 <kwonh11@gmail.com>
2020-09-13 14:36:21 +0900

    Docs : 프로젝트 계획서 수정

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const path = require('path');
+const cors = require('cors');
 require("dotenv").config();
 
 const indexRouter = require("./routes");
@@ -8,12 +10,14 @@ const connect = require("./schemas");
 const app = express();
 connect();
 
+
 app.set("port", process.env.PORT || 8000);
-
-app.use(morgan("dev"));
-
-app.use("/", indexRouter);
-
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 대기중!!!");
 });
+
+app.use(morgan("dev"));
+app.use(express.static(path.resolve(__dirname, 'dist')));
+
+
+
