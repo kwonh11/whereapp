acc3a1d03bd93d96449ccc5c453140f96f9dd502
kwonh11 <kwonh11@gmail.com>
2020-09-21 20:09:13 +0900

    Feat : 홈화면 카테고리 애니메이션

--- src/home/container/CategoryContainer.js
+++ src/home/container/CategoryContainer.js
@@ -7,12 +7,12 @@ const Container = styled.div`
   width: 100%;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
-  gap: 25px;
+  gap: 30px;
 `;
 const Rise = styled.div`
   opacity: ${props => props.rise === "1"? 1 : 0};
   margin-top: ${props => props.rise === "1"? "0px" : "30px"};
-  transition: all 0.4s ease-out ${props => props.index * 0.15}s;
+  transition: all 0.5s ease-out ${props => props.index * 0.15}s;
 `;
 
 export default function CategoryContainer() {
