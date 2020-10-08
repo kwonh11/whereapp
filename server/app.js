41fd8cc837dceb2c40ba2f14d26b3fa99a05c6e3
pgg-dev <park.geumgyu@gmail.com>
2020-10-08 16:35:59 +0900

    Feat : 에러 처리 미들웨어 추가

--- server/app.js
+++ server/app.js
@@ -18,9 +18,7 @@ connect();
 passportConfig(passport);
 
 app.set("port", process.env.PORT || 8000);
-app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번 대기중!!!");
-});
+
 app.use(middlewares.logger);
 app.use(morgan("dev"));
 app.use(express.json({ extended: true }));
@@ -46,3 +44,20 @@ app.use("/", express.static(path.join(__dirname, "view")));
 app.use("/auth", authRouter);
 app.use("/location", locationRouter);
 app.use("/place", placeRouter);
+
+app.use((req, res, next) => {
+  const err = new Error("Not Fount");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "devlopment" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 대기중!!!");
+});
