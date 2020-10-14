d420f8eb35efb5434ac57a7010dc2c6f20254e83
pgg-dev <park.geumgyu@gmail.com>
2020-10-15 00:04:04 +0900

    Feat : 게시글 좋아요(하트) 로그인 체크 추가

--- src/detail/container/AdditionalContainer.js
+++ src/detail/container/AdditionalContainer.js
@@ -4,7 +4,8 @@ import {
   Favorite as FavoriteIcon,
   Share as ShareIcon,
 } from "@material-ui/icons";
-import { IconButton } from "@material-ui/core";
+import { IconButton, Snackbar } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
 import Overview from "../component/Overview";
 import AdditionalComponent from "../component/Additional";
 import Map from "../component/Map";
@@ -42,7 +43,7 @@ export default function AdditionalContainer(props) {
   const { origin } = useSelector((state) => state.location);
   const { contentTypeId, contentId } = useSelector((state) => state.detail.ids);
   const { additional, isLoading, place } = useSelector((state) => state.detail);
-  const { hearts } = useSelector((state) => state.user);
+  const { hearts, isLoggedIn } = useSelector((state) => state.user);
   const isOnline = useSelector((state) =>
     state.detail.place.addr1.includes("온라인")
   );
@@ -58,6 +59,7 @@ export default function AdditionalContainer(props) {
   );
 
   const [isHeart, setIsHeart] = useState(false);
+  const [snack, setSnack] = React.useState(false);
 
   useEffect(() => {
     requestDetails(contentTypeId, contentId);
@@ -68,8 +70,16 @@ export default function AdditionalContainer(props) {
   }, []);
 
   const handleClickHeart = () => {
-    setIsHeart(!isHeart);
-    dispatch(actions.setHeartsRequest(place));
+    if (isLoggedIn) {
+      setIsHeart(!isHeart);
+      dispatch(actions.setHeartsRequest(place));
+    } else {
+      setSnack(true);
+    }
+  };
+
+  const handleCloseSnackbar = () => {
+    setSnack(false);
   };
 
   return (
@@ -91,9 +101,23 @@ export default function AdditionalContainer(props) {
             <HeartBtn onClick={handleClickHeart} heart={isHeart ? 1 : 0}>
               <FavoriteIcon style={{ width: "1.5rem", height: "1.5rem" }} />
             </HeartBtn>
-            <IconButton aria-label="share" onClick={() => console.log("share")}>
+            <IconButton onClick={() => console.log("share")}>
               <ShareIcon />
             </IconButton>
+            <Snackbar
+              open={snack}
+              onClose={handleCloseSnackbar}
+              anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            >
+              <Alert
+                onClose={handleCloseSnackbar}
+                severity="error"
+                variant="filled"
+                style={{ fontWeight: "bold" }}
+              >
+                로그인 후 이용해주세요.
+              </Alert>
+            </Snackbar>
           </ActionsWrap>
         </>
       )}
