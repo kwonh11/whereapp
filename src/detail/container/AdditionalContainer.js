5dec4927400c1cd3e1f30267f2bf46e080953e4f
kwonh11 <kwonh11@gmail.com>
2020-10-19 13:15:35 +0900

    임시 커밋

--- src/detail/container/AdditionalContainer.js
+++ src/detail/container/AdditionalContainer.js
@@ -4,8 +4,7 @@ import {
   Favorite as FavoriteIcon,
   Share as ShareIcon,
 } from "@material-ui/icons";
-import { IconButton, Snackbar } from "@material-ui/core";
-import { Alert } from "@material-ui/lab";
+import { IconButton } from "@material-ui/core";
 import Overview from "../component/Overview";
 import AdditionalComponent from "../component/Additional";
 import Map from "../component/Map";
@@ -63,11 +62,10 @@ export default function AdditionalContainer(props) {
   }, [contentId]);
 
   const handleClickHeart = () => {
+    if (!isLoggedIn) return;
     if (isLoggedIn) {
       setIsHeart(!isHeart);
       dispatch(userActions.setHeartsRequest(place));
-    } else {
-      setSnack(true);
     }
   };
 
