20efdd3edb93327ca99a0c7eecd286bf29f62ca8
kwonh11 <kwonh11@gmail.com>
2020-10-19 17:55:24 +0900

    Test: home saga 테스트 코드 작성 중

--- src/common/test/home.test.js
+++ src/common/test/home.test.js
@@ -12,10 +12,55 @@ describe("fetchBestList", () => {
     isHandledAddress: false,
     origin: {},
   };
-  const action = actions.requestBestPlaceList({ payload });
-  const gen = cloneableGenerator(fetchBestList)();
+  const dummyOrigin = {
+    lat: 37.502804,
+    lng: 126.9539593,
+  };
+
+  const action = actions.requestBestPlaceList(payload);
+  const { regionCode, isHandledAddress, origin } = action.payload;
+  const gen = cloneableGenerator(fetchBestList)({ payload });
   expect(gen.next().value).toEqual(put(actions.setLoading(true)));
   expect(gen.next().value).toEqual(put(actions.setError("")));
-  expect(gen.next().value).toEqual(call(getUsersLocation));
-  expect(gen.next().value).toEqual(call(getUsersLocation));
+
+  it("getUsersLocation API가 정상적으로 작동되는가", () => {
+    gen.next();
+    expect(call(getUsersLocation)).toHaveProperty("lng");
+    expect(call(getUsersLocation)).toHaveProperty("lat");
+  });
+  gen.next(); // setOrigin
+
+  it("callApiGetAddress API가 정상적으로 작동되는가", () => {
+    gen.next();
+    expect(call(callApiGetAddress, dummyOrigin)).data.toBe(
+      "대한민국 서울특별시 상도동 상도SH빌아파트상도팰리스후문"
+    );
+  });
+  gen.next(); // setAddress
+
+  it("callApBestList가 20개의 요소를 응답하는가", () => {
+    gen.next();
+    // bestListAPI가 20개를 리턴해주는가
+    expect(call(callApiBestList, regionCode)).data.toHaveLength(20);
+  });
+
+  it("placeList.map() 메서드가 return 되는가", () => {
+    gen.next().toHaveReturned(); // bestList.data.map()
+  });
+  gen.next(); // setBestPlaceList
+
+  it("실패 했을 경우", () => {
+    const errorGen = gen.clone();
+    const errorMsg = "error";
+    expect(errorGen.throw(errorMsg).value).toEqual(
+      put(actions.setError(errorMsg))
+    );
+  });
+  it("성공했을 경우", () => {
+    gen.next(); // delay
+    const successGen = gen.clone();
+    expect(successGen.next(Promise.resolve()).value).toEqual(
+      put(actions.setLoading(false))
+    );
+  });
 });
